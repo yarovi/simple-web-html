3c9db6fe8eba2523cf0c183b5bff96c0d29802ae
root <root@miw-2022-uo000000-ubuntu.localdomain>
2023-09-28 21:35:56 +0000

    completed upload csv

--- main.js
+++ main.js
@@ -1,46 +1,12 @@
-function uploadCsv() {
-    const inpObj = document.getElementById("my-csv");
-    const file = inpObj.files[0];
-    // console.log(file);
-    // const reader = new FileReader();
+document.getElementById('my-file-csv').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
 
-    // reader.onload = function(inpObj) {
-    //     const content = inpObj.target.result;
-    //     const rows = content.split('\n');
+    reader.onload = function(e) {
+        const content = e.target.result;
+        console.log(content);
+    };
 
-    //     const csvData = [];
-    //     rows.forEach(function(row) {
-    //         const cells = row.split(',');
-    //         csvData.push(cells);
-    //     });
-
-    //     console.log(csvData);
-    // };
-
-    // reader.readAsText(file);
-};
-
-/*
-document.getElementById('csvForm').addEventListener('submit',
-    function (event) {
-        event.preventDefault();
-
-
-        var formData = new FormData(this);
-
-        fetch('http://156.35.98.155:80', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.text())
-            .then(data => {
-                // Manipular la respuesta del servidor (opcional)
-                console.log(data);
-                
-                console.log(data.text);
-            })
-            .catch(error => console.error('Error:', error));
-
-
-    })
-    */
+    reader.readAsText(file);
+    document.getElementById("my-result").innerHTML = "Proceso OK";
+});
